265d26f012c35f05b07221c4fccaec73c3774685
Luciana Viviani Gallo <lucianavivianigallo@gmail.com>
2022-02-08 20:44:46 +0000

    Feature Spec: Product Details

---
+++ spec/features/product_details_spec.rb
@@ -0,0 +1,32 @@
+require 'rails_helper'
+
+RSpec.feature "ProductDetails", type: :feature do
+  # SETUP
+  before :each do
+    @category = Category.create! name: 'Apparel'
+
+    10.times do |n|
+      @category.products.create!(
+      name:  Faker::Hipster.sentence(3),
+      description: Faker::Hipster.paragraph(4),
+      image: open_asset('apparel1.jpg'),
+      quantity: 10,
+      price: 64.99
+      )
+    end
+
+    scenario "Check details of a product" do
+      visit root_path
+  
+      find('a.btn.btn-default').click
+      page.has_content?('Description')
+  
+      save_screenshot
+    
+    end
+
+  end
+
+
+
+end
