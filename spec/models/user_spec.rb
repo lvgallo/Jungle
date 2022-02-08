3c1c876d4a47a4f4990b655be77e0f008b35f6da
Luciana Viviani Gallo <lucianavivianigallo@gmail.com>
2022-02-08 01:36:52 +0000

    Define validation specs

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,39 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  describe 'Validations' do
+
+    it 'should be created successfully will all required fields' do 
+      @user = User.create(first_name: 'Lucy', last_name: 'Lee', email: 'lucy@lee.com', password:'lucylee123', password_confirmation: 'lucylee123')
+      expect(@user).to be_valid
+    end
+
+   it 'should fail if first name is missing' do 
+      @user = User.create(last_name: 'Lee', email: 'lucy@lee.com', password:'lucylee123', password_confirmation: 'lucylee123')
+      expect(@user).to be_invalid
+    end 
+
+    it 'should fail if last _name is missing' do 
+      @user = User.new(first_name: 'Lucy', email: 'lucy@lee.com', password:'lucylee123', password_confirmation: 'lucylee123')
+      @user.save
+      expect(@user).to be_invalid
+    end  
+
+    it 'should fail if email is missing' do 
+      @user = User.create(first_name: 'Lucy', last_name: 'Lee', password:'lucylee123', password_confirmation: 'lucylee123')
+      expect(@user).to be_invalid
+    end
+   
+    it 'should fail if password is different than password confirmation' do 
+      @user = User.create(first_name: 'Lucy', last_name: 'Lee', email: 'lucy@lee.com', password:'lucylee123', password_confirmation: 'lucylee456')
+      expect(@user).to be_invalid
+    end 
+
+    it 'should fail if email' do 
+      @user1 = User.create(first_name: 'Lucy', last_name: 'Lee', email: 'lucy@lee.com', password:'lucylee123', password_confirmation: 'lucylee123')
+      @user2 = User.create(first_name: 'Lulu', last_name: 'Leeson', email: 'LUCY@LEE.com', password:'lululee123', password_confirmation: 'lululee123')
+      expect(@user2).to be_invalid
+    end 
+
+  end
+end
